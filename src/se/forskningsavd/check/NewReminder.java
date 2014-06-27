6245e505e2162d0a49572dc81ee58d44a176d9fa
rymdhund <rymdhund@forskningsavd.se>
2014-06-27 13:04:14 +0200

    Color picker for reminder colors

--- src/se/forskningsavd/check/NewReminder.java
+++ src/se/forskningsavd/check/NewReminder.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import se.forskningsavd.check.database.ReminderDataSource;
 import se.forskningsavd.check.model.Reminder;
+
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -14,24 +16,27 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class NewReminder extends Activity {
+public class NewReminder extends Activity implements ColorPickerDialog.OnColorChangedListener{
 	private static final String TAG = "NewReminder";
-	
-	private Reminder reminder;
+    private static final String STATE_COLOR = "reminderColor";
+
+    private Reminder reminder;
+
+    private int color;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_reminder);
-		
-		Intent intent = getIntent();
+
+        Intent intent = getIntent();
 		reminder = (Reminder)intent.getSerializableExtra(EditFragment.EXTRA_REMINDER);
 		
 		if(reminder != null){
 			((EditText)findViewById(R.id.name_edittext)).setText(reminder.getName());
 			((EditText)findViewById(R.id.interval_edittext)).setText(""+reminder.getDayInterval());
-			((EditText)findViewById(R.id.max_count_edittext)).setText(""+reminder.getMaxCheckCount());
-            findViewById(R.id.color_view).setBackgroundColor(reminder.getColor());
+			((EditText)findViewById(R.id.max_count_edittext)).setText("" + reminder.getMaxCheckCount());
+            color = reminder.getColor();
             if(reminder.getDayInterval() != 0){
                 ((CheckBox)findViewById(R.id.interval_checkbox)).setChecked(true);
                 findViewById(R.id.text1).setEnabled(true);
@@ -41,8 +46,23 @@ public class NewReminder extends Activity {
 		}else{
 			((EditText)findViewById(R.id.interval_edittext)).setText("1");
 			((EditText)findViewById(R.id.max_count_edittext)).setText("1");
-            findViewById(R.id.color_view).setBackgroundColor(randomColor());
-		}
+            color = randomColor();
+        }
+
+        if (savedInstanceState != null) {
+            // Restore color from saved state
+            color = savedInstanceState.getInt(STATE_COLOR);
+        }
+
+        findViewById(R.id.color_view).setBackgroundColor(color);
+
+        final NewReminder parent = this;
+        findViewById(R.id.color_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ColorPickerDialog(parent, parent, color).show();
+            }
+        });
 	}
 
     public void onIntervalCheckBoxClick(View view){
@@ -74,13 +94,11 @@ public class NewReminder extends Activity {
         }
 
 		int maxCount = Integer.parseInt(
-				((EditText)findViewById(R.id.max_count_edittext)).getText().toString());
-        int color = findViewById(R.id.color_view).getDrawingCacheBackgroundColor();
-		
+                ((EditText) findViewById(R.id.max_count_edittext)).getText().toString());
+
 		Reminder r = new Reminder(name, interval, 0, maxCount, color);
 		if(reminder != null){
 			r.setDbId(reminder.getDbId());
-			r.setColor(reminder.getColor());
 		}
 		ReminderDataSource rds = new ReminderDataSource(this);
 		rds.open();
@@ -96,4 +114,15 @@ public class NewReminder extends Activity {
 		return r.nextInt();
 	}
 
+    @Override
+    public void colorChanged(int color) {
+        this.color = color;
+        findViewById(R.id.color_view).setBackgroundColor(color);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt(STATE_COLOR, color);
+        super.onSaveInstanceState(savedInstanceState);
+    }
 }
